0a26bdd8b258cfc04831893ea4b2351fd596a932
Matej Arlović <matej.arlovic@gmail.com>
2020-02-11 16:44:23 +0100

    Added feature extract into four threads

--- feature_mining.py
+++ feature_mining.py
@@ -1,5 +1,6 @@
 import os
 import re
+from joblib import Parallel, delayed
 import AudioFFmpeg as ffmpeg
 import AudioFeatures as af
 
@@ -8,22 +9,24 @@ song_directory = 'media/songs/'
 csv_directory = 'media/dataset.csv'
 segments_len = 30
 id = 1
+n_jobs = 4
 
-## Get All Songs
-for song in list(os.walk(song_directory))[1::]:
+## Process Songs
+def process_songs(song):
+    global id
     song_directory = song[0]
-    song_full_name = ''.join(song[2])
+    song_full_name = song[2][0]
     song_path = song_directory + "/" + song_full_name
-    song = ffmpeg.AudioFFmpeg()
+    ffmpeg_song = ffmpeg.AudioFFmpeg()
 
     ## Generate Segments
-    length = song.get_audio_length(song_path)
+    length = ffmpeg_song.get_audio_length(song_path)
     if length > 30:
         try:
             if song[2][1] is None:
                 pass
         except IndexError:
-            song.generate_audio_segments(song_path, song_directory, segments_len)        
+            ffmpeg_song.generate_audio_segments(song_path, song_directory, segments_len)        
 
     ## Get Audio File Features
     segments = round(round(length / segments_len) / 2)
@@ -35,3 +38,6 @@ for song in list(os.walk(song_directory))[1::]:
     song_full_name = re.sub(regex, "", song_full_name)
     song_features.format_song_features(id, song_path_info[2], song_full_name, length, (segments, segments + 1))
     id = id + 1
+
+## Paralel Feature Extraction
+Parallel(n_jobs=n_jobs, prefer="threads")(delayed(process_songs)(song) for song in list(os.walk(song_directory))[1::])
