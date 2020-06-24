6b7382572c41142e84554ac87c7269be849ecc0a
franjojosip <franjojosip.jukic2@gmail.com>
2020-06-24 10:16:52 +0200

    Refactor and change project files

--- music-research-ui/src/common/RootStore.js
+++
@@ -1,8 +0,0 @@
-import { RouterState, RouterStore } from 'mobx-state-router';
-import { routes } from './Routes';
-
-const notFound = new RouterState('notFound');
-
-export default class RootStore {
-    routerStore = new RouterStore(this, routes, notFound);
-}
---
+++ music-research/build/precache-manifest.10ba920e9e3c394220a8ebd476507369.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49561a4623eebce3bc45492ecacc8805",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb6b6b6b2983b68dd8b1",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "5e6b108f5c6ff0d91854",
+    "url": "/static/css/main.9a6cdc6a.chunk.css"
+  },
+  {
+    "revision": "bb6b6b6b2983b68dd8b1",
+    "url": "/static/js/2.b3a32713.chunk.js"
+  },
+  {
+    "revision": "f20be6eb17ccb1d5da32d5f9409bfae5",
+    "url": "/static/js/2.b3a32713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e6b108f5c6ff0d91854",
+    "url": "/static/js/main.efaa6c6b.chunk.js"
+  },
+  {
+    "revision": "c41e06483224cb1df1ee",
+    "url": "/static/js/runtime-main.c148df72.js"
+  },
+  {
+    "revision": "82e56549885549d3f9b1a8aa0bd69a0a",
+    "url": "/static/media/blues.82e56549.jpg"
+  },
+  {
+    "revision": "3d24e287058d72a6780e6357e0b8f6ad",
+    "url": "/static/media/classical.3d24e287.jfif"
+  },
+  {
+    "revision": "0aed69991f9277510e3c4e2cade37856",
+    "url": "/static/media/country.0aed6999.jfif"
+  },
+  {
+    "revision": "df316454bd0c2685fc9589be2864847e",
+    "url": "/static/media/dance.df316454.jfif"
+  },
+  {
+    "revision": "ff068903c119796ef87d620dd2c4d580",
+    "url": "/static/media/disco.ff068903.jpg"
+  },
+  {
+    "revision": "e7f845988444b3da73b73000f387b456",
+    "url": "/static/media/hiphop.e7f84598.jfif"
+  },
+  {
+    "revision": "61fb3538277dc633be73b5b279756d6a",
+    "url": "/static/media/jazz.61fb3538.jpg"
+  },
+  {
+    "revision": "0e0906fa1b078f925d987643b80fe2ed",
+    "url": "/static/media/metal.0e0906fa.jfif"
+  },
+  {
+    "revision": "3176c40c9997a0459e71908213f181b1",
+    "url": "/static/media/pop.3176c40c.png"
+  },
+  {
+    "revision": "42eba885d0e2cdff935c37308f657a9f",
+    "url": "/static/media/reggae.42eba885.jfif"
+  },
+  {
+    "revision": "9b8bbf81be5e537289ddd7047d2d5bfc",
+    "url": "/static/media/rock.9b8bbf81.jfif"
+  },
+  {
+    "revision": "164b4cb3a7e6babb3c7cab7bd5aec625",
+    "url": "/static/media/techno.164b4cb3.jfif"
+  },
+  {
+    "revision": "789ea0b1c2dedaabdae5d05819a2663c",
+    "url": "/static/media/trance.789ea0b1.jpg"
+  }
+]);
